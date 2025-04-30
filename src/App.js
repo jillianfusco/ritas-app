c76ef7dc000e32f8a6c8c192a119ff1692b17568
Jillian Fusco <fuscojillian@gmail.com>
2025-04-30 16:47:31 -0400

    renders nutrition message in display nutrition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
+import React, { useState } from 'react';
 import Form from './components/Form.js';
+import DisplayNutrition from './components/DisplayNutrition.js';
 
 function App() {
+  const [itemType, setItemType] = useState('');
+  const [flavor, setFlavor] = useState('');
+  const [size, setSize] = useState('');
+  const [summary, setSummary] = useState('');
+  const [nutrition, setNutrition] = useState(null);
+
   return (
     <div >
-      <Form />
+       <Form 
+        itemType={itemType}
+        setItemType={setItemType}
+        flavor={flavor}
+        setFlavor={setFlavor}
+        size={size}
+        setSize={setSize}
+        summary={summary}
+        setSummary={setSummary}
+        setNutrition={setNutrition}
+      />
+
+      <DisplayNutrition 
+      itemType={itemType}
+      setItemType={setItemType}
+      flavor={flavor}
+      setFlavor={setFlavor}
+      size={size}
+      setSize={setSize}
+      summary={summary}
+      setSummary={setSummary}
+      setNutrition={setNutrition}
+      />
+
     </div>
   );
 }
