a9dcaecca2e7b3c4f77b7593c692624ba7eca747
Jillian Fusco <fuscojillian@gmail.com>
2025-04-30 13:58:32 -0400

    Added select tag and options to form

---
+++ src/components/Form.js
@@ -0,0 +1,25 @@
+
+function Form () {
+    return(
+        <form>
+            <h1>Rita's Nutritonal Information</h1>
+            <label>Choose an item: </label>
+            <select name="item" id="item">
+                <option value="water-ice">Water Ice</option>
+                <option value="soft-serve">Soft Serve</option>
+                <option value="gelati">Gelati</option>
+                <option value="blendini">Blendini</option>
+                <option value="concrete">Concrete</option>
+                <option value="ice-blender">Ice Blender</option>
+                <option value="gelati-blender">Gelati Blender</option>
+                <option value="frozen-coffee">Frozen Coffee</option>
+                <option value="frozen-lemonade">Frozen Lemonade</option>
+                <option value="cookie-sandwich">Cookie Sandwich</option>
+                <option value="pup-cup">Pup Cup</option>                
+            </select>
+        </form>
+    )
+    
+}
+
+export default Form
