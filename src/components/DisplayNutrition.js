853ff5a01a9113e454b81f476d92fa6bf137469e
Jillian Fusco <fuscojillian@gmail.com>
2025-05-05 16:55:42 -0400

    WaterIceForm.js now displays flavors and sizes dynamically based on what is in WaterIceData.json

--- src/components/DisplayNutrition.js
+++ src/components/DisplayNutrition.js
@@ -1,15 +1,21 @@
-import WaterIceData from '../data/WaterIceDummyData.json';
+import WaterIceData from '../data/CompleteWaterIceData.json';
+
 
 function DisplayNutrition({ itemType, flavor, size }) {
     if (!itemType || !flavor || !size) return null;
 
-    const normalize = (str) => str.toLowerCase().replace(/\s+/g, '-');
+    const normalize = (value) =>
+        value
+          .split('-')
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ');
+    
 
     const item = WaterIceData.find(
     (entry) =>
-        normalize(entry.type) === itemType &&
-        normalize(entry.flavor) === flavor &&
-        normalize(entry.size) === size
+        entry.type === normalize(itemType) &&
+        entry.flavor === normalize(flavor) &&
+        entry.size === normalize(size)
     );
 
     if (!item) return <p>No nutrition info found for this selection</p>;
@@ -19,16 +25,17 @@ function DisplayNutrition({ itemType, flavor, size }) {
             <p>Here's the nutrition information for a {item.size} {item.flavor} {item.type}: </p>
             <ul>
                 <li>Calories: {item.calories}</li>
-                <li>Total Fat: {item.totalFat}g</li>
-                <li>Saturated Fat: {item.saturatedFat}g</li>
-                <li>Trans Fat: {item.transFat}g</li>
+                <li>Total Fat: {item.fat}g</li>
+                <li>Saturated Fat: {item.saturated_fat}g</li>
+                <li>Trans Fat: {item.trans_fat}g</li>
                 <li>Cholesterol: {item.cholesterol}mg</li>
                 <li>Sodium: {item.sodium}mg</li>
-                <li>Total Carbohydrates: {item.totalCarbohydrates}g</li>
-                <li>Sugars: {item.sugars}g</li>
-                <li>Added Sugars: {item.addedSugars}g</li>
+                <li>Total Carbohydrates: {item.total_carbohydrates}g</li>
+                <li>Sugars: {item.total_sugar}g</li>
+                <li>Added Sugars: {item.added_sugar}g</li>
                 <li>Protein: {item.protein}g</li>
             </ul>
+
         </div>
     );
 }
